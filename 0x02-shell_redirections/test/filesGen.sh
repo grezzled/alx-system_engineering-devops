ea0e790ae77f7d3a0fdded9ce78a63046b614de4
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-03 06:30:18 -0700

    extra

---
+++ 0x02-shell_redirections/test/filesGen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#!/bin/bash
+
+# Create directories
+mkdir -p 0x02-shell_redirections/0-commas-checks
+mkdir -p 0x02-shell_redirections/javascript
+mkdir -p 0x02-shell_redirections/test_dir/photos
+
+# Create empty files
+touch 0x02-shell_redirections/0-commas
+touch 0x02-shell_redirections/1-empty_casks
+touch 0x02-shell_redirections/2-gifs
+touch 0x02-shell_redirections/3-directories
+touch 0x02-shell_redirections/4-zeros
+touch 0x02-shell_redirections/5-rot13
+touch 0x02-shell_redirections/6-odd
+touch 0x02-shell_redirections/7-sort_rot13
+touch 0x02-shell_redirections/........gif
+touch 0x02-shell_redirections/..hello.gif
+touch 0x02-shell_redirections/Kris_is_awesome\ \:\)
+touch 0x02-shell_redirections/Makefile
+touch 0x02-shell_redirections/Rona_napping.gif
+touch 0x02-shell_redirections/root.gif
+touch 0x02-shell_redirections/..something
+touch 0x02-shell_redirections/test_dir/docker.gif
+touch 0x02-shell_redirections/test_dir/file.sh
+touch 0x02-shell_redirections/test_dir/.horrible_selfie.gif
+touch 0x02-shell_redirections/test_dir/photos/cat.gif
+touch 0x02-shell_redirections/test_dir/photos/index.html
+touch 0x02-shell_redirections/test_dir/photos/main.gif
+touch 0x02-shell_redirections/test_dir/photos/rudy_rigot.gif
